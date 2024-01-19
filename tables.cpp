257597d9f7c1c3ba1e3b93e4b23074bd7c96cc7d
SohamGalande <ssgalande045@gmail.com>
2024-01-19 09:18:17 +0530

    Code For Table

---
+++ tables.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int i,num;
+	cout<<"Enter the number for table";
+	cin>>num;
+
+	for(i=1;i<=10;i++)
+	{
+	cout<<num<<"*"<<i<<"is"<<num*i<<endl;
+	}
+
+
+
+}
